c380280af069deeec96e5d77d1b533df250cf0a9
kmochi-05 <kmochi005515@gmail.com>
2026-03-22 23:43:28 +0900

    入力フォームとデータ受け取り機能を追加

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/add", methods=["POST"])
+def add():
+    subject = request.form["subject"]
+    time = request.form["time"]
+    date = request.form["date"]
+
+    return f"科目: {subject}, 時間: {time}, 日付: {date}"
 
 if __name__ == "__main__":
     app.run(debug=True)
