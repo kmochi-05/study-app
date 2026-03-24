272dc722063cc9dee1825f4ecd56b6b43e5dad6b
kmochi-05 <kmochi005515@gmail.com>
2026-03-24 14:08:26 +0900

    学習記録をSQLiteに保存する機能を追加

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
 from flask import Flask, render_template, request
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect("study.db")
+    c = conn.cursor()
+
+    c.execute(""""
+              CREATE TABLE IF NOT EXISTS records (
+              id INTEGER PRIMARY KEY AUTOINCREMENT, 
+              subject TEXT
+              time INTGER,
+              date TEXT
+              )
+              """)
+    
+    conn.commit()
+    conn.close()
+
 
 app = Flask(__name__)
 
@@ -12,7 +30,20 @@ def add():
     time = request.form["time"]
     date = request.form["date"]
 
-    return f"科目: {subject}, 時間: {time}, 日付: {date}"
+    conn = sqlite3.connect("study.db")
+    c = conn.cursor()
+
+    c.execute("INSERT INTO records (subject, time, date) VALUES (?, ?, ?)",
+              (subject, time, date))
+    
+    conn.commit()
+    conn.close()
+
+    return "保存しました"
 
 if __name__ == "__main__":
+    app.run(debug=True)
+
+if __name__ == "__main__":
+    init_db()
     app.run(debug=True)
